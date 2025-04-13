b55eb8d1348de04f05dac55fb4ea91805e782bcb
Polina <alllrrw@gmail.com>
2025-04-13 15:57:42 +0700

    feat: add solutions

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -111,8 +111,15 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return result.value;
+      }
+      return null;
+    });
+  });
 }
 
 /**
@@ -133,8 +140,12 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((chain, promise) => {
+    return chain.then((result) => {
+      return promise.then((value) => result + value);
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
